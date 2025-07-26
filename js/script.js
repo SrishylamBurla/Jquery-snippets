99abb00db751447372c6ad74aeb02180176054b2
srishylam burla <srishylamburla1@gmail.com>
2025-07-26 22:55:09 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$("document").ready(function() {
+            $("body").append("<h1>I love jQuery!</h1>");
+            $("body").append("<p>This is a paragraph added by jQuery.</p>");            
+        });
